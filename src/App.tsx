d080332a1a42e57b8530c31ea422e7798a7f79f5
StingStorm <alexkalganov18@gmail.com>
2025-03-13 00:23:19 +0200

    setup packages. update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
   return (
-    <div>
+    <div className="h-[400px] bg-sky-400">
       <p>Welcome to Luna Edge technical interview</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
