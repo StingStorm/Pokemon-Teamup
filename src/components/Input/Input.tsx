9a239698a854900cac7555d0dbcb38a2234e23b9
StingStorm <alexkalganov18@gmail.com>
2025-03-14 13:36:24 +0200

    tiny fix

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -13,7 +13,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         className={`min-h-8 px-3 py-1 border rounded-[0.25rem] ${
           error
             ? 'border-red-500 shadow-[0_0_0_1px_rgb(239,68,68)]'
-            : 'border-gray-400  has-[:hover]:border-purple-800 has-[:hover]:shadow-[0_0_0_1px_rgb(107,33,168)] has-[:focus]:border-purple-800 has-[:focus]:shadow-[0_0_0_1px_rgb(107,33,168)]'
+            : 'border-gray-400  hover:border-purple-800 hover:shadow-[0_0_0_1px_rgb(107,33,168)] focus:border-purple-800 focus:shadow-[0_0_0_1px_rgb(107,33,168)]'
         }`}
         type="text"
         ref={ref}
